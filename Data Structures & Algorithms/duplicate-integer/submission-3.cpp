759df2463e509c8d4adfaa5346ba62c38923e849
kdh0106 <64049714+kdh0106@users.noreply.github.com>
2026-08-04 22:31:06 +0900

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        if(nums.size() == 0 || nums.size() == 1)
+            return false;
+
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size() - 1; i++){
+            if(nums[i] == nums[i+1])
+                return true;
+        }
+        return false;
+    }
+};
